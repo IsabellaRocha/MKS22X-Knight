39f7bafd7afd7c334a59d1652148e7f437b1c1f4
Isabella Rocha <irocha00@stuy.edu>
2019-02-13 20:39:47 -0500

    Got solve to work

--- KnightBoard.java
+++ KnightBoard.java
@@ -37,7 +37,7 @@ public class KnightBoard {
     if (startingRow < 0 || startingCol < 0 || startingRow >= board.length || startingCol >= board[0].length) {
       throw new IllegalArgumentException();
     }
-    return solveH(0, 0, 1);
+    return solveH(startingRow, startingCol, 1);
   }
   private boolean solveH(int row, int col, int moveNumber) {
     if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) {
@@ -52,12 +52,19 @@ public class KnightBoard {
         return false;
       }
     }
+    boolean check = false;
     if (board[row][col] == 0 && moveNumber < board.length * board[0].length) {
       board[row][col] = moveNumber;
       for (int idx = 0; idx <= 7; idx++) {
         if (row + Poss[idx][0] >= 0 && col + Poss[idx][1] >= 0 && row + Poss[idx][0] < board.length && col + Poss[idx][1] < board[0].length) {
-          solveH(row + Poss[idx][0], col + Poss[idx][1], moveNumber + 1);
+          if(solveH(row + Poss[idx][0], col + Poss[idx][1], moveNumber + 1)) {
+            check = true;
+            return true;
+          }
         }
+      }
+      if (check == false) {
+        board[row][col] = 0;
       }
     }
     return false;
